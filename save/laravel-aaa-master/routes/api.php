743802710634c921428a9b5f66bf31425d36eb6f
mm-khsv <khmari329@gmail.com>
2024-03-24 13:23:32 +0330

    use sanctum auth

---
+++ save/laravel-aaa-master/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use dnj\AAA\Http\Controllers\TypesController;
+use dnj\AAA\Http\Controllers\UsersController;
+use dnj\AAA\Http\Middleware\UpdateOnlineTimeOfUsers;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix("v1")->middleware(["api", "auth:sanctum", UpdateOnlineTimeOfUsers::class])->group(function() {
+	Route::apiResource("users", UsersController::class);
+	Route::apiResource("types", TypesController::class);
+});
